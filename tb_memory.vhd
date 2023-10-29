00f2339d4f901595c149823cba7f67a082aac8e4
rafael_torres_nantes <rafatorresnantes@gmail.com>
2023-10-28 23:33:40 -0400

    Update Final LH - Trab Part 1

--- tb_memory.vhd
+++ tb_memory.vhd
@@ -5,7 +5,6 @@ use ieee.numeric_std.all;
 entity tb_memory is
 end entity;
 
-
 architecture mix of tb_memory is
     constant addr_width: natural := 16; -- Memory Address Width (in bits)
     constant data_width: natural := 8; -- Data Width (in bits)
@@ -39,7 +38,7 @@ begin
             data_out : std_logic_vector((data_width*4)-1 downto 0);
         end record;
 
-        type vetor_tv is array (0 to 11) of line_tabela_verdade;
+        type vetor_tv is array (0 to 17) of line_tabela_verdade;
         constant tabela_verdade : vetor_tv := (
          
         -- Tabela Verdade Corrigida (falling_edge)
@@ -51,25 +50,17 @@ begin
             ('1', '1', '0', x"000F", x"0000", x"00123400"), -- Print [xF, 10, 11, 12] : 00123400, out : 00123400
             ('0', '1', '0', x"000F", x"0000", x"00123400"), -- Print [xF, 10, 11, 12] : 00123400, out : 00123400
             ('1', '1', '0', x"000E", x"0000", x"12340000"), -- Print [xE, xF, 10, 11] : 12340000, out : 12340000
-            ('0', '1', '0', x"000E", x"0000", x"12340000"), -- Print [xE, xF, 10, 11] : 12340000, out : 12340000
-            ('1', '0', '1', x"000E", x"5566", x"00000000"), -- Não faz nada (pq não é falling_edge), out : 0
-            ('0', '0', '1', x"000E", x"5566", x"00000000"), -- Escreve [xE, xF, 10, 11] : 12345566, out : 0
-            ('1', '1', '0', x"000E", x"0000", x"12345566"), -- Print [xE, xF, 10, 11] : 12345566, out : 12345566
-            ('0', '1', '0', x"000E", x"0000", x"12345566")  -- Print [xE, xF, 10, 11] : 12345566, out : 12345566
-
-        -- Tabela Verdade do Veterano 
-        -- -- clock, d_r, d_w, adress, data_in, r: data_out
-        --     ('1', '0', '1', x"0000" , x"00ff",  x"00000000"),  --testa escrita
-        --     ('0', '0', '1', x"0000" , x"00ff",  x"00000000"),  
-        --     ('1', '0', '1', x"0000" , x"00ff",  x"00000000"),
-        --     ('0', '0', '1', x"0001" , x"0011",  x"00000000"),  
-        --     ('1', '0', '1', x"0001" , x"0011",  x"00000000"),
-        --     ('0', '0', '1', x"0002" , x"0000",  x"00000000"),  
-        --     ('1', '0', '1', x"0002" , x"0000",  x"00000000"),
-        --     ('0', '0', '1', x"0003" , x"0011",  x"00000000"),  
-        --     ('1', '0', '1', x"0003" , x"0011",  x"00000000"),
-        --     ('0', '1', '0', x"0000" , x"0000",  x"110011ff"),  --testa leitura
-        --     ('0', '1', '0', x"0000" , x"0000",  x"110011ff")	
+            ('0', '1', '0', x"000D", x"0000", x"34000000"), -- Print [xD, xE, xF, 10] : 34000000, out : 34000000
+            ('1', '1', '0', x"0011", x"0000", x"00000012"), -- Print [11, 12, 13, 14] : 00000012, out : 00000012
+            ('0', '1', '0', x"0012", x"0000", x"00000000"), -- Print [12, 13, 14, 15] : 00000000, out : 00000000
+            ('1', '0', '1', x"000E", x"5678", x"00000000"), -- Não faz nada (pq não é falling_edge), out : 0
+            ('0', '0', '1', x"000E", x"9123", x"00000000"), -- Escreve [xE, xF, 10, 11] : 12349101, out : 0
+            ('1', '1', '0', x"000E", x"0000", x"12349123"), -- Print [xE, xF, 10, 11] : 12349123, out : 12349123
+            ('0', '1', '0', x"000E", x"0000", x"12349123"),  -- Print [xE, xF, 10, 11] : 12349123, out : 12349123
+            ('1', '0', '1', x"0010", x"4567", x"00000000"), -- Não faz nada (pq não é falling_edge), out : 0
+            ('0', '0', '1', x"0010", x"4567", x"00000000"), -- Escreve [10, 11, 12, 13] : 45679101, out : 0
+            ('1', '1', '0', x"000E", x"0000", x"45679123"), -- Print [xE, xF, 10, 11] : 45679123, out : 45679123
+            ('0', '1', '0', x"0010", x"0000", x"00004567")  -- Print [10, 11, 12, 13] : 45679123, out : 00004567
         );
 
     begin
@@ -83,8 +74,9 @@ begin
             wait for 1 ns;
 
             assert data_out = tabela_verdade(i).data_out 
-                report "Erro resultado! " &
-                integer'image(i) & " != " &
+                report "ERROR : Valor não correspondente na tabela verdade. Linha[" &
+                integer'image(i) & "], o resultado deveria ser: " &
+                integer'image(to_integer(signed(tabela_verdade(i).data_out))) & " != " &
                 integer'image(to_integer(signed(data_out)))
             severity failure;
             
